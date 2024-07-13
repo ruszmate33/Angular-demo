ec0b94c1ee31de3cfbc05ca288a23d039dddf509
Mate Rusz <ruszmate33@gmail.com>
2024-07-13 07:20:05 +0200

    Storing Data Models in Separate Files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -14,7 +9,7 @@ interface User {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true}) user!: User;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
